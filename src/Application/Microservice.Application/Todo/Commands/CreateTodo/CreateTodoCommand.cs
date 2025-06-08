18c6a4ebddca8c0215287d8f04171d3de6bf5f00
tumiwisista <pawpilichowski@gmail.com>
2025-06-08 18:32:27 +0200

    docs: Add comprehensive user documentation - Add complete README.md with features, quick start, and examples - Add GETTING_STARTED.md guide for new users - Clean up generated CLI artifacts - Remove unnecessary test projects and duplicated files - Prepare for gradual development plan implementation

--- src/Application/Microservice.Application/Todo/Commands/CreateTodo/CreateTodoCommand.cs
+++
@@ -1,3 +0,0 @@
-namespace Microservice.Application.Todo.Commands.CreateTodo;
-
-public record CreateTodoCommand(string Title);
--- src/Application/Microservice.Application/Todo/Queries/GetTodos/GetTodosQuery.cs
+++
@@ -1,5 +0,0 @@
-using Microservice.Domain.Entities;
-
-namespace Microservice.Application.Todo.Queries.GetTodos;
-
-public record GetTodosQuery();
